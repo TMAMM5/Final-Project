287145eae6f2a44b21cb65c15ff454f7cff9fdf4
momran0123s <102372842+momran0123s@users.noreply.github.com>
2024-03-18 22:46:00 +0200

    orderRepo

---
+++ Final Project/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using Final_Project.Repository.OrderRepo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project.Controllers
+{
+    public class OrderController : Controller
+    {
+        IOrderRepository _orderRepository;
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
