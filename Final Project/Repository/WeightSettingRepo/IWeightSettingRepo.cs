f2616bff9c7d8b093cd52dd7ec76efbe5f88184f
tarek <tarek.khaled3996@gmail.com>
2024-03-19 17:36:21 +0200

    T-edit

---
+++ Final Project/Repository/WeightSettingRepo/IWeightSettingRepo.cs
@@ -0,0 +1,12 @@
+﻿using Final_Project.Models;
+
+namespace Final_Project.Repository.WeightSettingRepo
+{
+    public interface IWeightSettingRepo
+    {
+        IEnumerable<WeightSetting> GetAll();
+        WeightSetting GetById(int id);
+        void Save();
+        void Update(WeightSetting weightSetting);
+    }
+}
