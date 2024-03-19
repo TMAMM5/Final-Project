10d17e4df74859887c458e6e38941e3acc6e6d13
tarek <tarek.khaled3996@gmail.com>
2024-03-19 17:56:06 +0200

    t-try

---
+++ Final Project/Repository/DiscountTypeRepo/DiscountTypeRepository.cs
@@ -0,0 +1,46 @@
+﻿using Final_Project.Models;
+namespace Final_Project.Repository.DiscountTypeRepo
+{
+    public class DiscountTypeRepository : IDiscountTypeRepository
+    {
+
+        private ProjContext _context;
+        public DiscountTypeRepository(ProjContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(DiscountType discount)
+        {
+            _context.DiscountTypes.Add(discount);
+        }
+
+        public void Delete(int id)
+        {
+            DiscountType discount = GetById(id);
+            _context.DiscountTypes.Remove(discount);
+
+        }
+
+        public void Edit(DiscountType discount)
+        {
+            _context.Entry(discount).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        }
+
+        public List<DiscountType> GetAll()
+        {
+            return _context.DiscountTypes.ToList();
+        }
+
+        public DiscountType GetById(int id)
+        {
+            return _context.DiscountTypes.Find(id);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}
---
+++ Final Project/Repository/DiscountTypeRepo/IDiscountTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Final_Project.Models;
+
+namespace Final_Project.Repository.DiscountTypeRepo
+{
+    public interface IDiscountTypeRepository
+    {
+        void Add(DiscountType discount);
+        void Delete(int id);
+        void Edit(DiscountType discount);
+        List<DiscountType> GetAll();
+        DiscountType GetById(int id);
+        void Save();
+    }
+}
