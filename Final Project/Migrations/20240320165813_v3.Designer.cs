fbba19605afe3a8b5a43014bafaa7d592becf8f8
Mahmoud Usama <mahmoudusamaseraj96@gmail.com>
2024-03-22 01:40:41 +0200

    role , employee controller and views

--- Final Project/Migrations/20240320165813_v3.Designer.cs
+++ Final Project/Migrations/20240320165813_v3.Designer.cs
@@ -4,6 +4,7 @@ using Final_Project.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Final_Project.Migrations
 {
     [DbContext(typeof(ProjContext))]
-    partial class ProjContextModelSnapshot : ModelSnapshot
+    [Migration("20240320165813_v3")]
+    partial class v3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Final Project/ViewModel/UserVM.cs
@@ -0,0 +1,18 @@
+﻿using Final_Project.Models;
+
+namespace Final_Project.ViewModel
+{
+    public class UserVM
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime creationDate { get; set; }
+        public virtual Branch? Branch { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Role { get; set; }
+    }
+}
