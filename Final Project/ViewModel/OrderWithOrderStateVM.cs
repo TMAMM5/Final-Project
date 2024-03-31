64e4ce425f7af173f8be9f6a9e16e3513128521e
momran0123s <102372842+momran0123s@users.noreply.github.com>
2024-03-31 02:01:22 +0200

    order-and-representative-functionality

---
+++ Final Project/ViewModel/OrderWithOrderStateVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Final_Project.ViewModel
+{
+    public class OrderWithOrderStateVM
+    {
+        public int OrderID { get; set; }
+        public int? OrderStateId { get; set; }
+    }
+}
